4de3f00357940c9e045b9a0aa18568bc4ea850e7
Paul Evans <decretist@gmail.com>
2023-03-15 16:02:38 -0700

    Ingobert2 now runs under Python 3.11/Django 4.1

--- ingobert/urls.py
+++ ingobert/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import url
+from django.urls import path
 from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="ingobert/index.html")),
-    url(r'demo/', views.demo),
-    url(r'home/', views.home),
-    url(r'2column/', views.two_column),
-    url(r'3column/', views.three_column),
-    url(r'4column/', views.four_column),
+    path('', TemplateView.as_view(template_name='ingobert/index.html')),
+    path('demo/', views.demo),
+    path('home/', views.home),
+    path('2column/', views.two_column),
+    path('3column/', views.three_column),
+    path('4column/', views.four_column),
 ]
 
